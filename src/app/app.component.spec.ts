d48f724dcbe16a5f16c69ca82374ebf33aec67ba
Edmilson Matias <matiasjr@live.com>
2025-12-31 11:11:53 -0300

    Ajustes na pagina de projetos, deixando mais ornizado e sem o botao novo ficar sobreposto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent], // AppComponent é standalone
     }).compileComponents();
   });
 
@@ -14,16 +14,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'bndes-projetos-web' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('bndes-projetos-web');
-  });
-
-  it('should render title', () => {
+  it('should render router-outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
+
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, bndes-projetos-web');
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
